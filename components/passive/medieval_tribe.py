c5b7370a55a93394d3786a2225d1e759b3ebc97b
Pannawich Kangphanich <6810110200@psu.ac.th>
2026-03-02 22:51:38 +0700

    [Deleted] medieval_pawn_passive - ย้าย logic ไปที่ Pawn class

--- components/passive/medieval_tribe.py
+++ components/passive/medieval_tribe.py
@@ -1,66 +1,4 @@
-# Medieval Tribe Components
-
-def medieval_pawn_passive():
-    """
-    Pawn passive ability for Medieval tribe
-    - First move: can move 2 squares straight
-    - Subsequent moves: 1 square straight only
-    - Starting stats: 2 dice, 1 coin
-    - Max stats: 12
-    """
-    def get_valid_moves(start, end, board):
-        """
-        Check if move is valid for Medieval Pawn
-        start: start position (row, col)
-        end: target position (row, col)
-        board: game board state
-        """
-        sr, sc = start
-        er, ec = end
-        
-        # ดึง piece จาก board
-        piece = board[sr][sc]
-        if not piece or piece.name.lower() != 'p':
-            return False
-            
-        direction = -1 if piece.color == 'white' else 1
-        
-        # การเดินทางตรง
-        if sc == ec:
-            # First move: can move 1 or 2 squares
-            if not piece.has_moved:
-                # Move 1 square
-                if er == sr + direction and 0 <= er < 8 and not board[er][ec]:
-                    return True
-                # Move 2 squares
-                if er == sr + 2*direction and 0 <= er < 8 and not board[er][ec] and not board[sr + direction][sc]:
-                    return True
-            else:
-                # Subsequent moves: only 1 square
-                if er == sr + direction and 0 <= er < 8 and not board[er][ec]:
-                    return True
-        
-        # การกินแนวทแยง
-        elif abs(sc - ec) == 1 and er == sr + direction:
-            target = board[er][ec]
-            if target and target.color != piece.color:
-                return True
-                
-        return False
-    
-    def get_piece_stats():
-        """
-        Get starting stats for Medieval Pawn
-        """
-        return {
-            'dice': 2,
-            'coins': 1
-        }
-    
-    return {
-        'get_valid_moves': get_valid_moves,
-        'get_piece_stats': get_piece_stats
-    }
+# Medieval Tribe - ความสามารถพิเศษของเผ่า Medieval
 
 def medieval_knight_passive():
     """
@@ -84,6 +22,10 @@ def medieval_knight_passive():
         if not piece or piece.name.lower() != 'n':
             return False
             
+        # ตรวจสอบขอบเขตกระดาน
+        if not (0 <= er < 8 and 0 <= ec < 8):
+            return False
+            
         # คำนวณระยะทาง
         row_diff = abs(sr - er)
         col_diff = abs(sc - ec)
@@ -103,12 +45,11 @@ def medieval_knight_passive():
     
     def get_piece_stats():
         """
-        Get starting and max stats for Medieval Knight
+        Get starting stats for Medieval Knight
         """
         return {
             'dice': 2,
-            'coins': 1,
-            'max_stats': 12
+            'coins': 1
         }
     
     return {
